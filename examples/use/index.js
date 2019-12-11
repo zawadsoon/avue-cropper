e2cf6423ecc711e3c982621f27cbe79a1e0ade47
Marcin Zawada <zawadsoon@gmail.com>
2019-12-11 19:14:27 +0100

    removed vuex dependency

--- examples/use/index.js
+++ examples/use/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import AVueCropper from '@/avue-cropper';
 
 import App from './App.vue';
 
-Vue.use(Vuex);
 Vue.use(AVueCropper);
 
 new Vue({
   el: '#app',
-  store: new Vuex.Store(),
   render: createElement => createElement(App)
 });
